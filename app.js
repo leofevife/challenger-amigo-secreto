7b31bb267a2bd0cffb9fbb55478386e257d1db0f
leofevife <leofevife@gmail.com>
2025-02-25 11:33:46 -0300

    Função para adicionar amigo

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+let pessoas = [];
 
 
 
+function nomeNoCampoDeTexto () {
+    let inputNome = document.getElementById('amigo').value;
+
+    if (inputNome === "") {
+        alert("Insira um nome!");
+        return;
+    }
+    amigos.push(inputNome);
+    document.getElementById('amigo').value = "";
+    atualizarListaAmigos();
+}
 
 
 
