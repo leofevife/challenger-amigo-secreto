8acd2f4289b5da92e125deacb43198d7386a0524
leofevife <leofevife@gmail.com>
2025-02-25 12:02:32 -0300

    Resultado final

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-let pessoas = [];
+
+let amigos = [];
 
 
-
-function nomeNoCampoDeTexto () {
-    let inputNome = document.getElementById('amigo').value;
+function addAmigo() {
+    const inputNome = document.getElementById('amigo').value;
 
     if (inputNome === "") {
-        alert("Insira um nome!");
+        alert("Por favor, insira um nome.");
         return;
     }
+
     amigos.push(inputNome);
     document.getElementById('amigo').value = "";
     atualizarListaAmigos();
@@ -16,36 +17,35 @@ function nomeNoCampoDeTexto () {
 
 
 function atualizarListaAmigos() {
-    let listaAmigos = document.getElementById('listaAmigos');
+    const listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = "";
 
     amigos.forEach(function(amigo) {
-        let novoAmigo = document.createElement('li');
+        const novoAmigo = document.createElement('li');
         novoAmigo.textContent = amigo;
         listaAmigos.appendChild(novoAmigo);
     });
 }
 
 
-function sorteioIndividual() {
+function sorteioInd() {
     if (amigos.length < 2) {
-        alert("Adicione pelo menos 2 amigos para sortear.");
+        alert("Por favor, adicione pelo menos 2 amigos para sortear.");
         return;
     }
 
     let amigosEmbaralhados = amigos.slice();
 
     for (let i = amigosEmbaralhados.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
+        const j = Math.floor(Math.random() * (i + 1));
         [amigosEmbaralhados[i], amigosEmbaralhados[j]] = [amigosEmbaralhados[j], amigosEmbaralhados[i]];
     }
 
-}
-
-let amigoSorteado = amigosEmbaralhados[0];
-
-let resultado = document.getElementById('resultado');
+    
+    const amigoSorteado = amigosEmbaralhados[0];  
+    const resultado = document.getElementById('resultado');
     resultado.innerHTML = `Amigo sorteado: ${amigoSorteado}`;
+}
 
 
 
